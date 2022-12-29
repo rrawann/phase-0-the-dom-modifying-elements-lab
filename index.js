431cff7016519b049333d67f5a6d4596ad0b1ebf
Rawan <rawnq.sb@gmail.com>
2022-12-30 01:00:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'rawan is the champion';
